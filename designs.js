fdd8a459189f80437ac9de53662704379660cefe
Michele Wallace <45398272+MWalKnox@users.noreply.github.com>
2019-05-09 19:44:59 -0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,4 +1,4 @@
-function makeGrid() {	
+function makeGrid() { // a grid should appear when the user inputs numbers and clicks submit
     var gridHeight = $('#userHeight').val(); // User input sets grid height
     var gridWidth = $('#userWidth').val(); // User input sets grid width
     var canvas = $('#canvas');
@@ -9,7 +9,7 @@ function makeGrid() {
     canvas.children().remove(); // Draw an empty grid on page load and reset when Submit is clicked
 
 	// Refer to numerical input from the user for Height
-	if ($('#userHeight').val() > 25) gridHeight = 25;  // hard stop at 25 x 25
+	if ($('#userHeight').val() > 25) gridHeight = 25; // hard stop at 25 x 25
 	document.getElementById("userHeight").value = gridHeight; // checks the number value and changes it to the current grid height if value is higher than 25
     for (var rowCount = 0; rowCount < gridHeight; rowCount++) { // adds to row count to meet user request
 		canvas.append('<tr></tr>'); // Creates more cells for height
@@ -17,7 +17,7 @@ function makeGrid() {
 
 	// Refer to numerical input from the user for Width
 	newRow = $('tr');
-	if ($('#userWidth').val() > 25) gridWidth = 25;  // hard stop at 25 x 25
+	if ($('#userWidth').val() > 25) gridWidth = 25; // hard stop at 25 x 25
 	document.getElementById("userWidth").value = gridWidth; // checks the number value and changes it to the current grid width if value is higher than 25
     for (var numCell = 0; numCell < gridWidth; numCell++) { // adds to cell count to meet user request
 		newRow.append('<td></td>'); // Creates more cells for width
